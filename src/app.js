e936585ae2ee802c215d23f5df3bac23e96ed366
Maeva <maevashali@gmail.com>
2021-09-05 18:36:20 +0300

    Added the forecast HTML element using JSf

--- src/app.js
+++ src/app.js
@@ -21,6 +21,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="http://openweathermap.org/img/wn/04d@2x.png" 
+            alt="Overcast cloud"
+            width="55px">
+            <div class="wether-forecast-temperature">
+              <span class="weather-forecast-temperature-max">18°</span> <span class ="weather-forecast-temperature-min">11°</span>
+            </div>
+
+          
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input-name");
@@ -105,3 +130,4 @@ geolocationButton.addEventListener("click", displayCurrentLocation);
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 search("Ambanitsena");
+displayForecast();
